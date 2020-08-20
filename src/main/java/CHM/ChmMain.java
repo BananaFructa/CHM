ab7743328473674b3264146b1084f053bde5cf42
Banana Fructa <55049439+BananaFructa@users.noreply.github.com>
2020-08-20 17:14:06 +0300

    chm main

---
+++ src/main/java/CHM/ChmMain.java
@@ -0,0 +1,18 @@
+package CHM;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ChmMain.MODID, version = ChmMain.VERSION)
+public class ChmMain
+{
+    public static final String MODID = "chm";
+    public static final String VERSION = "0.1";
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+}
